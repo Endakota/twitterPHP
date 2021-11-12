ec28fa306255974e8024b8fe4b49ef75325da51a
Endakota <endakota09@gmail.com>
2021-11-12 23:07:21 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+    // константы
+    define("SITE_NAME","Twitter");
+    // конкат-я это через " . "
+    define("HOST", "http://" . $_SERVER["HTTP_HOST"]);
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "twitter");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+?>
